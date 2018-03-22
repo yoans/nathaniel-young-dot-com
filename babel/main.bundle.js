2a124dbfdb901b1932ad6c5dbd02b7eaa7dfb175
Nathaniel Young <NY15855@johndeere.com>
2018-03-22 17:07:52 -0500

    progress

--- babel/main.bundle.js
+++ babel/main.bundle.js
@@ -1,11 +1,13 @@
 'use strict';
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 let demo = (() => {
   var _ref = _asyncToGenerator(function* () {
     let cyclingGrid = seedGrid();
     while (true) {
-      cyclingGrid = seedGrid();
-      yield sleep(1000);
+      cyclingGrid = nextGrid(cyclingGrid);
+      yield sleep(10000);
       _reactDom2.default.render(Application(cyclingGrid), document.getElementById('root'));
     }
   });
@@ -39,43 +41,91 @@ function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, a
 
 const chance = new _chance2.default();
 const vectors = ['arrow-up', 'arrow-left', 'arrow-down', 'arrow-right'];
+const vectorOperations = [{ func: ({ x, y }) => ({ x, y: y + 1 }) }, { func: ({ x, y }) => ({ x: x - 1, y }) }, { func: ({ x, y }) => ({ x, y: y - 1 }) }, { func: ({ x, y }) => ({ x: x + 1, y }) }];
 const getVector = () => chance.natural({
   min: 0,
   max: 3
 });
-const cycleVector = vector => {
-  return (vector + 1) % 4;
+const cycleVector = (vector, number) => {
+  return (vector + number - 1) % 4;
 };
-
 const getRandomNumber = size => chance.natural({
   min: 0,
   max: size - 1
 });
-
 const getGrid = size => ({
   rows: R.range(0, size).map(() => R.range(0, size))
 });
-
 const getArrow = size => () => ({
   x: getRandomNumber(size),
   y: getRandomNumber(size),
   vector: getVector()
 });
-
 const newGrid = (size, numberOfArrows) => {
   const arrows = R.range(0, numberOfArrows).map(getArrow(size));
 
   return Object.assign(getGrid(size), { arrows });
 };
-
 const seedGrid = () => newGrid(getRandomNumber(20) + 1, getRandomNumber(100) + 1);
-
+const moveArrow = arrow => {
+  return _extends({}, arrow, vectorOperations[arrow.vector].func(arrow));
+};
+const arrowKey = arrow => '{x:' + arrow.x + ',y:' + arrow.y + '}';
+const arrowBoundaryKey = (arrow, size) => {
+  if (arrow.x === size && arrow.vector === 3) {
+    return 3;
+  }
+  if (arrow.y === size && arrow.vector === 2) {
+    return 2;
+  }
+  if (arrow.x === 0 && arrow.vector === 1) {
+    return 1;
+  }
+  if (arrow.y === 0 && arrow.vector === 0) {
+    return 0;
+  }
+  return 'no-boundary';
+};
+const newArrayIfFalsey = thingToCheck => thingToCheck ? thingToCheck : [];
+const rotateArrow = number => arrow => _extends({}, arrow, {
+  vector: cycleVector(arrow.vector, number)
+});
+const rotateSet = set => set.map(rotateArrow(set.length));
 const nextGrid = grid => {
-
   const size = grid.rows.length;
   const arrows = grid.arrows;
-  arrows;
-  return grid;
+  const newGrid = {
+    rows: getGrid(size).rows,
+    arrows: []
+  };
+
+  const arrowSetDictionary = arrows.reduce((arrowDictionary, arrow) => {
+    arrowDictionary[arrowKey(arrow)] = [...newArrayIfFalsey(arrowDictionary[arrowKey(arrow)]), arrow];
+    return arrowDictionary;
+  }, {});
+
+  const arrowSets = Object.keys(arrowSetDictionary).map(key => arrowSetDictionary[key]);
+  const rotatedArrows = arrowSets.map(rotateSet);
+  const flatRotatedArrows = rotatedArrows.reduce((accum, current) => [...accum, ...current], []);
+
+  const arrowBoundaryDictionary = flatRotatedArrows.reduce((arrowDictionary, arrow) => {
+    arrowDictionary[arrowBoundaryKey(arrow, size)] = [...newArrayIfFalsey(arrowDictionary[arrowBoundaryKey(arrow, size)]), arrow];
+    return arrowDictionary;
+  }, {});
+
+  const movedArrowsInMiddle = newArrayIfFalsey(arrowBoundaryDictionary['no-boundary']).map(moveArrow);
+  const movedBoundaryArrows = vectorOperations.map((operation, key) => {
+    console.log(arrowBoundaryDictionary[(key + 2) % 4]);
+    console.log(newArrayIfFalsey(arrowBoundaryDictionary[(key + 2) % 4]));
+    console.log(operation);
+
+    console.log({ key });
+    console.log({ mod: (key + 2) % 4 });
+
+    return newArrayIfFalsey(arrowBoundaryDictionary[(key + 2) % 4]).map(operation.func);
+  });
+  newGrid.arrows = [...movedArrowsInMiddle, ...movedBoundaryArrows];
+  return newGrid;
 };
 
 const renderItem = item => {
--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Chance from 'chance';
@@ -12,44 +10,113 @@ const vectors = [
   'arrow-down',
   'arrow-right'
 ];
+const vectorOperations = [
+  {func:({x, y})=>({x, y: y+1})},
+  {func:({x, y})=>({x: x-1, y})},
+  {func:({x, y})=>({x, y: y-1})},
+  {func:({x, y})=>({x: x+1, y})}
+];
 const getVector = () => chance.natural({
   min: 0,
   max: 3
 });
-
-const cycleVector = (vector) => {
-  return (vector + 1) % 4
-}
-
+const cycleVector = (vector, number) => {
+  return (vector + number - 1) % 4
+};
 const getRandomNumber = (size) => chance.natural({
   min: 0,
   max: size - 1
 });
-
 const getGrid = (size) => ({
   rows: R.range(0, size).map(() => R.range(0, size))
 });
-
 const getArrow = size => () => ({
   x: getRandomNumber(size),
   y: getRandomNumber(size),
   vector: getVector()
 });
-
 const newGrid = (size, numberOfArrows) => {
   const arrows = R.range(0, numberOfArrows).map(getArrow(size))
 
   return Object.assign(getGrid(size), {arrows});
-}
-
+};
 const seedGrid = () => newGrid(getRandomNumber(20)+1, getRandomNumber(100)+1);
-
+const moveArrow = arrow => {
+  return {
+    ...arrow,
+    ...vectorOperations[arrow.vector].func(arrow)
+  }
+};
+const arrowKey = arrow => '{x:'+arrow.x+',y:'+arrow.y+'}';
+const arrowBoundaryKey = (arrow, size)=> {
+  if(arrow.x === size && arrow.vector === 3) {
+    return 3;
+  }
+  if(arrow.y === size && arrow.vector === 2) {
+    return 2;
+  }
+  if(arrow.x === 0 && arrow.vector === 1) {
+    return 1;
+  }
+  if(arrow.y === 0 && arrow.vector === 0) {
+    return 0;
+  }
+  return 'no-boundary';
+};
+const newArrayIfFalsey = thingToCheck => thingToCheck ? thingToCheck : [];
+const rotateArrow = number => arrow => ({
+  ...arrow,
+  vector: cycleVector(arrow.vector, number)
+});
+const rotateSet = set => set.map(rotateArrow(set.length));
 const nextGrid = (grid) => {
-
   const size = grid.rows.length;
   const arrows = grid.arrows;
-  // arrows.map();
-  return grid;
+  const newGrid = {
+    rows: getGrid(size).rows,
+    arrows:[]
+  };
+
+  const arrowSetDictionary = arrows.reduce(
+      (arrowDictionary, arrow) => {
+        arrowDictionary[arrowKey(arrow)] = [
+          ...(newArrayIfFalsey(arrowDictionary[arrowKey(arrow)])),
+          arrow
+        ];
+        return arrowDictionary;
+      }
+  ,{});
+
+  const arrowSets = Object.keys(arrowSetDictionary).map(key => arrowSetDictionary[key]);
+  const rotatedArrows = arrowSets.map(rotateSet);
+  const flatRotatedArrows = rotatedArrows.reduce((accum, current)=>[...accum, ...current],[]);
+
+  const arrowBoundaryDictionary = flatRotatedArrows.reduce(
+      (arrowDictionary, arrow) => {
+        arrowDictionary[arrowBoundaryKey(arrow, size)] = [
+          ...(newArrayIfFalsey(arrowDictionary[arrowBoundaryKey(arrow, size)])),
+          arrow
+        ];
+        return arrowDictionary;
+      }
+      ,{});
+
+  const movedArrowsInMiddle = newArrayIfFalsey(arrowBoundaryDictionary['no-boundary']).map(moveArrow);
+  const movedBoundaryArrows = vectorOperations.map((operation, key) => {
+    console.log(arrowBoundaryDictionary[(key+2)%4]);
+    console.log(newArrayIfFalsey(arrowBoundaryDictionary[(key+2)%4]));
+    console.log(operation)
+
+    console.log({key});
+    console.log({mod: (key+2)%4});
+
+    return newArrayIfFalsey(arrowBoundaryDictionary[(key+2)%4]).map(operation.func)
+  });
+  newGrid.arrows = [
+    ...movedArrowsInMiddle,
+    ...movedBoundaryArrows
+  ];
+  return newGrid;
 };
 
 const renderItem = (item) => {
@@ -73,7 +140,7 @@ const renderItem = (item) => {
       <div className={'empty-space'}/>
     </td>
     )
-}
+};
 
 const renderRow = (row, index) => {
   return (
@@ -81,7 +148,7 @@ const renderRow = (row, index) => {
       {row.map(renderItem)}
     </tr>
   )
-}
+};
 const renderGrid = (grid) => {
   
   const populateArrow = x => y => grid.arrows.filter(arrow => arrow.x===x && arrow.y===y);
@@ -112,9 +179,8 @@ async function demo() {
   let cyclingGrid = seedGrid();
   while(true){
     cyclingGrid = nextGrid(cyclingGrid);
-    await sleep(1000);
+    await sleep(10000);
     ReactDOM.render(Application(cyclingGrid), document.getElementById('root'));
-
   }
 }
 
