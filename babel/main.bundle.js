97ddf43730a651ee86cd449d60f02d1737bbec15
Nathaniel Young <nathanyoans@gmail.com>
2018-03-24 16:42:51 -0500

    Animations (#1)
    * progress
    
    * bug fixes
    
    * added git ignore
    
    * tests
    
    * test
    
    * working demo
    
    * added animations
    
    * removing node_modules
    
    * converted to class, fixed animation added inputs

--- babel/main.bundle.js
+++ babel/main.bundle.js
@@ -1,19 +1,11 @@
 'use strict';
 
-let demo = (() => {
-  var _ref = _asyncToGenerator(function* () {
-    let cyclingGrid = seedGrid();
-    while (true) {
-      cyclingGrid = seedGrid();
-      yield sleep(1000);
-      _reactDom2.default.render(Application(cyclingGrid), document.getElementById('root'));
-    }
-  });
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Application = exports.nextGrid = exports.flipArrow = exports.rotateSet = exports.rotateArrow = exports.newArrayIfFalsey = exports.arrowBoundaryKey = exports.arrowKey = exports.moveArrow = exports.seedGrid = exports.newGrid = exports.getArrow = exports.getRows = exports.getRandomNumber = exports.cycleVector = exports.getVector = exports.vectorOperations = exports.vectors = undefined;
 
-  return function demo() {
-    return _ref.apply(this, arguments);
-  };
-})();
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 var _react = require('react');
 
@@ -35,110 +27,308 @@ function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj;
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
 
 const chance = new _chance2.default();
-const vectors = ['arrow-up', 'arrow-left', 'arrow-down', 'arrow-right'];
-const getVector = () => chance.natural({
-  min: 0,
-  max: 3
-});
-const cycleVector = vector => {
-  return (vector + 1) % 4;
+const vectors = exports.vectors = ['arrow-up', 'arrow-right', 'arrow-down', 'arrow-left'];
+const vectorOperations = exports.vectorOperations = [function ({ x, y, vector }) {
+  return { x, y: y - 1, vector };
+}, function ({ x, y, vector }) {
+  return { x: x + 1, y, vector };
+}, function ({ x, y, vector }) {
+  return { x, y: y + 1, vector };
+}, function ({ x, y, vector }) {
+  return { x: x - 1, y, vector };
+}];
+const getVector = exports.getVector = function () {
+  return chance.natural({
+    min: 0,
+    max: 3
+  });
 };
-
-const getRandomNumber = size => chance.natural({
-  min: 0,
-  max: size - 1
-});
-
-const getGrid = size => ({
-  rows: R.range(0, size).map(() => R.range(0, size))
-});
-
-const getArrow = size => () => ({
-  x: getRandomNumber(size),
-  y: getRandomNumber(size),
-  vector: getVector()
-});
-
-const newGrid = (size, numberOfArrows) => {
+const cycleVector = exports.cycleVector = function (vector, number) {
+  return (vector + number - 1) % 4;
+};
+const getRandomNumber = exports.getRandomNumber = function (size) {
+  return chance.natural({
+    min: 0,
+    max: size - 1
+  });
+};
+const getRows = exports.getRows = function (size) {
+  return R.range(0, size).map(function () {
+    return R.range(0, size);
+  });
+};
+const getArrow = exports.getArrow = function (size) {
+  return function () {
+    return {
+      x: getRandomNumber(size),
+      y: getRandomNumber(size),
+      vector: getVector()
+    };
+  };
+};
+const newGrid = exports.newGrid = function (size, numberOfArrows) {
   const arrows = R.range(0, numberOfArrows).map(getArrow(size));
 
-  return Object.assign(getGrid(size), { arrows });
+  return { size, arrows };
+};
+const seedGrid = exports.seedGrid = function () {
+  return newGrid(getRandomNumber(20) + 12, getRandomNumber(50) + 1);
+};
+const moveArrow = exports.moveArrow = function (arrow) {
+  return vectorOperations[arrow.vector](arrow);
+};
+const arrowKey = exports.arrowKey = function (arrow) {
+  return '{x:' + arrow.x + ',y:' + arrow.y + '}';
+};
+const arrowBoundaryKey = exports.arrowBoundaryKey = function (arrow, size) {
+  if (arrow.y === 0 && arrow.vector === 0) {
+    return 'boundary';
+  }
+  if (arrow.x === size - 1 && arrow.vector === 1) {
+    return 'boundary';
+  }
+  if (arrow.y === size - 1 && arrow.vector === 2) {
+    return 'boundary';
+  }
+  if (arrow.x === 0 && arrow.vector === 3) {
+    return 'boundary';
+  }
+  return 'no-boundary';
+};
+const newArrayIfFalsey = exports.newArrayIfFalsey = function (thingToCheck) {
+  return thingToCheck ? thingToCheck : [];
+};
+const rotateArrow = exports.rotateArrow = function (number) {
+  return function (arrow) {
+    return _extends({}, arrow, {
+      vector: cycleVector(arrow.vector, number)
+    });
+  };
+};
+const rotateSet = exports.rotateSet = function (set) {
+  return set.map(rotateArrow(set.length));
+};
+const flipArrow = function (_ref) {
+  let { vector } = _ref,
+      rest = _objectWithoutProperties(_ref, ['vector']);
+
+  return _extends({ vector: (vector + 2) % 4 }, rest);
 };
 
-const seedGrid = () => newGrid(getRandomNumber(20) + 1, getRandomNumber(100) + 1);
-
-const nextGrid = grid => {
-
-  const size = grid.rows.length;
+exports.flipArrow = flipArrow;
+const nextGrid = exports.nextGrid = function (grid) {
+  const size = grid.size;
   const arrows = grid.arrows;
-  arrows;
-  return grid;
+
+  const arrowSetDictionary = arrows.reduce(function (arrowDictionary, arrow) {
+    arrowDictionary[arrowKey(arrow)] = [...newArrayIfFalsey(arrowDictionary[arrowKey(arrow)]), arrow];
+    return arrowDictionary;
+  }, {});
+
+  const arrowSets = Object.keys(arrowSetDictionary).map(function (key) {
+    return arrowSetDictionary[key];
+  });
+  const rotatedArrows = arrowSets.map(rotateSet);
+  const flatRotatedArrows = rotatedArrows.reduce(function (accum, current) {
+    return [...accum, ...current];
+  }, []);
+
+  const arrowBoundaryDictionary = flatRotatedArrows.reduce(function (arrowDictionary, arrow) {
+    arrowDictionary[arrowBoundaryKey(arrow, size)] = [...newArrayIfFalsey(arrowDictionary[arrowBoundaryKey(arrow, size)]), arrow];
+    return arrowDictionary;
+  }, {});
+
+  const movedArrowsInMiddle = newArrayIfFalsey(arrowBoundaryDictionary['no-boundary']).map(moveArrow);
+  const movedFlippedBoundaryArrows = newArrayIfFalsey(arrowBoundaryDictionary['boundary']).map(flipArrow).map(moveArrow);
+
+  return {
+    size,
+    arrows: [...movedArrowsInMiddle, ...movedFlippedBoundaryArrows]
+  };
 };
 
-const renderItem = item => {
-  if (item.length == 1) {
+const renderItem = function (item) {
+  if (item.length) {
+    const classes = R.uniqBy(function (x) {
+      return x.vector;
+    }, item).map(function ({ vector }) {
+      return vectors[vector];
+    });
+
     return _react2.default.createElement(
       'td',
       null,
-      _react2.default.createElement('div', { className: vectors[item[0].vector] })
-    );
-  } else if (item.length) {
-    const classes = R.uniqBy(x => x.vector, item).map(({ vector }) => vectors[vector]).join(' ');
-    return _react2.default.createElement(
-      'td',
-      null,
-      _react2.default.createElement('div', { className: classes })
+      _react2.default.createElement(
+        'div',
+        { className: 'space' },
+        classes.map(function (divClass) {
+          return _react2.default.createElement('div', { className: divClass });
+        })
+      )
     );
   }
   return _react2.default.createElement(
     'td',
     null,
-    _react2.default.createElement('div', { className: 'empty-space' })
+    _react2.default.createElement('div', { className: 'space' })
   );
 };
 
-const renderRow = (row, index) => {
+const renderRow = function (row, index) {
   return _react2.default.createElement(
     'tr',
     { key: index.toString() },
     row.map(renderItem)
   );
 };
-const renderGrid = grid => {
+const renderGrid = function (grid) {
 
-  const populateArrow = x => y => grid.arrows.filter(arrow => arrow.x === x && arrow.y === y);
-  const populateRow = (row, index) => row.map(populateArrow(index));
+  const populateArrow = function (y) {
+    return function (x) {
+      return grid.arrows.filter(function (arrow) {
+        return arrow.x === x && arrow.y === y;
+      });
+    };
+  };
+  const populateRow = function (row, index) {
+    return row.map(populateArrow(index));
+  };
 
-  const populatedGrid = grid.rows.map(populateRow);
+  const populatedGrid = getRows(grid.size).map(populateRow);
   return populatedGrid.map(renderRow);
 };
+// const onChangeSize = (event) => 
 
-const Application = grid => _react2.default.createElement(
-  'div',
-  null,
-  _react2.default.createElement(
-    'h1',
-    null,
-    'Arrows'
-  ),
-  _react2.default.createElement('br', null),
-  _react2.default.createElement('br', null),
-  _react2.default.createElement(
-    'table',
-    null,
-    _react2.default.createElement(
-      'tbody',
+class Application extends _react2.default.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gridSize: 10,
+      numberOfArows: 10,
+      grid: newGrid(10, 10),
+      playing: true
+    };
+    this.newSizeHandler = this.newSize.bind(this);
+    this.newNumberOfArrowsHandler = this.newNumberOfArrows.bind(this);
+    this.nextGridHandler = this.nextGrid.bind(this);
+    this.newGridHandler = this.newGrid.bind(this);
+    this.playHandler = this.play.bind(this);
+    this.pauseHandler = this.pause.bind(this);
+  }
+
+  componentDidMount() {
+    this.playHandler();
+  }
+  play() {
+    var _this = this;
+
+    this.timerID = setInterval(function () {
+      return _this.nextGridHandler();
+    }, 500);
+    {
+      playing: true;
+    }
+  }
+  pause() {
+    clearInterval(this.timerID);
+    this.setState({ playing: false });
+  }
+  newSize(e) {
+    this.setState({
+      gridSize: parseInt(e.target.value)
+    });
+  }
+  newNumberOfArrows(e) {
+    this.setState({
+      numberOfArows: parseInt(e.target.value)
+    });
+  }
+  nextGrid() {
+    this.setState({
+      grid: nextGrid(this.state.grid)
+    });
+  }
+  newGrid() {
+    this.setState({
+      grid: newGrid(this.state.gridSize, this.state.numberOfArows)
+    });
+  }
+  render() {
+
+    return _react2.default.createElement(
+      'div',
       null,
-      renderGrid(grid)
-    )
-  )
-);
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+      _react2.default.createElement(
+        'h1',
+        null,
+        'Arrows'
+      ),
+      _react2.default.createElement('br', null),
+      _react2.default.createElement(
+        'p',
+        null,
+        'Size:'
+      ),
+      _react2.default.createElement('input', { type: 'number', onChange: this.newSizeHandler }),
+      _react2.default.createElement('br', null),
+      _react2.default.createElement(
+        'p',
+        null,
+        'Number:'
+      ),
+      _react2.default.createElement('input', { type: 'number', onChange: this.newNumberOfArrowsHandler }),
+      _react2.default.createElement('br', null),
+      _react2.default.createElement(
+        'button',
+        { onClick: this.newGridHandler },
+        'Reset'
+      ),
+      _react2.default.createElement(
+        'button',
+        { onClick: this.nextGridHandler },
+        'Next'
+      ),
+      _react2.default.createElement(
+        'button',
+        { disabled: this.state.playing, onClick: this.playHandler },
+        'Play'
+      ),
+      _react2.default.createElement(
+        'button',
+        { onClick: this.pauseHandler },
+        'Pause'
+      ),
+      _react2.default.createElement('br', null),
+      _react2.default.createElement(
+        'table',
+        null,
+        _react2.default.createElement(
+          'tbody',
+          null,
+          renderGrid(this.state.grid)
+        )
+      )
+    );
+  }
 }
+exports.Application = Application; // function sleep(ms) {
+//   return new Promise(resolve => setTimeout(resolve, ms));
+// }
 
-demo();
+// async function demo() {
+//   let cyclingGrid = seedGrid();
+//   while(true){
+//     cyclingGrid = nextGrid(cyclingGrid);
+//     await sleep(500);
+//     ReactDOM.render(Application(cyclingGrid), document.getElementById('root'));
+//   }
+// }
+
+// demo();
+
+_reactDom2.default.render(_react2.default.createElement(Application, null), document.getElementById('root'));
--- index.js
+++ index.js
@@ -1,79 +1,127 @@
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Chance from 'chance';
 import * as R from 'ramda';
 
 const chance = new Chance();
-const vectors = [
+export const vectors = [
   'arrow-up',
-  'arrow-left',
+  'arrow-right',
   'arrow-down',
-  'arrow-right'
+  'arrow-left'
 ];
-const getVector = () => chance.natural({
+export const vectorOperations = [
+  ({x, y, vector})=>({x, y: y-1, vector}),
+  ({x, y, vector})=>({x: x+1, y, vector}),
+  ({x, y, vector})=>({x, y: y+1, vector}),
+  ({x, y, vector})=>({x: x-1, y, vector})
+];
+export const getVector = () => chance.natural({
   min: 0,
   max: 3
 });
-
-const cycleVector = (vector) => {
-  return (vector + 1) % 4
-}
-
-const getRandomNumber = (size) => chance.natural({
+export const cycleVector = (vector, number) => {
+  return (vector + number - 1) % 4
+};
+export const getRandomNumber = (size) => chance.natural({
   min: 0,
   max: size - 1
 });
-
-const getGrid = (size) => ({
-  rows: R.range(0, size).map(() => R.range(0, size))
-});
-
-const getArrow = size => () => ({
+export const getRows = size => R.range(0, size).map(() => R.range(0, size));
+export const getArrow = size => () => ({
   x: getRandomNumber(size),
   y: getRandomNumber(size),
   vector: getVector()
 });
-
-const newGrid = (size, numberOfArrows) => {
+export const newGrid = (size, numberOfArrows) => {
   const arrows = R.range(0, numberOfArrows).map(getArrow(size))
 
-  return Object.assign(getGrid(size), {arrows});
-}
+  return {size, arrows};
+};
+export const seedGrid = () => newGrid(getRandomNumber(20)+12, getRandomNumber(50)+1);
+export const moveArrow = arrow => vectorOperations[arrow.vector](arrow);
+export const arrowKey = arrow => '{x:'+arrow.x+',y:'+arrow.y+'}';
+export const arrowBoundaryKey = (arrow, size)=> {
+  if(arrow.y === 0 && arrow.vector === 0) {
+    return 'boundary';
+  }
+  if(arrow.x === size - 1 && arrow.vector === 1) {
+    return 'boundary';
+  }
+  if(arrow.y === size - 1 && arrow.vector === 2) {
+    return 'boundary';
+  }
+  if(arrow.x === 0 && arrow.vector === 3) {
+    return 'boundary';
+  }
+  return 'no-boundary';
+};
+export const newArrayIfFalsey = thingToCheck => thingToCheck ? thingToCheck : [];
+export const rotateArrow = number => arrow => ({
+  ...arrow,
+  vector: cycleVector(arrow.vector, number)
+});
+export const rotateSet = set => set.map(rotateArrow(set.length));
+export const flipArrow = ({vector, ...rest}) => ({vector: (vector+2)%4, ...rest});
 
-const seedGrid = () => newGrid(getRandomNumber(20)+1, getRandomNumber(100)+1);
-
-const nextGrid = (grid) => {
-
-  const size = grid.rows.length;
+export const nextGrid = (grid) => {
+  const size = grid.size;
   const arrows = grid.arrows;
-  // arrows.map();
-  return grid;
+
+  const arrowSetDictionary = arrows.reduce(
+      (arrowDictionary, arrow) => {
+        arrowDictionary[arrowKey(arrow)] = [
+          ...(newArrayIfFalsey(arrowDictionary[arrowKey(arrow)])),
+          arrow
+        ];
+        return arrowDictionary;
+      }
+  ,{});
+
+  const arrowSets = Object.keys(arrowSetDictionary).map(key => arrowSetDictionary[key]);
+  const rotatedArrows = arrowSets.map(rotateSet);
+  const flatRotatedArrows = rotatedArrows.reduce((accum, current)=>[...accum, ...current],[]);
+
+  const arrowBoundaryDictionary = flatRotatedArrows.reduce(
+      (arrowDictionary, arrow) => {
+        arrowDictionary[arrowBoundaryKey(arrow, size)] = [
+          ...(newArrayIfFalsey(arrowDictionary[arrowBoundaryKey(arrow, size)])),
+          arrow
+        ];
+        return arrowDictionary;
+      }
+      ,{});
+
+  const movedArrowsInMiddle = newArrayIfFalsey(arrowBoundaryDictionary['no-boundary']).map(moveArrow);
+  const movedFlippedBoundaryArrows = newArrayIfFalsey(arrowBoundaryDictionary['boundary']).map(flipArrow).map(moveArrow);
+
+    return {
+        size,
+        arrows: [
+            ...movedArrowsInMiddle,
+            ...movedFlippedBoundaryArrows
+        ]
+    };
 };
 
 const renderItem = (item) => {
-  if(item.length == 1){
+  if(item.length){
+    const classes = R.uniqBy(x=>x.vector, item).map(({vector})=>vectors[vector]);
+
     return (
       <td>
-        <div className={vectors[item[0].vector]}/>
-      </td>
-    )
-  }
-  else if(item.length){
-      const classes = R.uniqBy(x=>x.vector, item).map(({vector})=>vectors[vector]).join(' ');
-    return (
-      <td>
-        <div className={classes}/>
+          <div className={'space'}>
+              {classes.map(divClass=>(<div className={divClass}/>))}
+          </div>
       </td>
     )
   }
   return (
     <td>
-      <div className={'empty-space'}/>
+      <div className={'space'}/>
     </td>
     )
-}
+};
 
 const renderRow = (row, index) => {
   return (
@@ -81,42 +129,117 @@ const renderRow = (row, index) => {
       {row.map(renderItem)}
     </tr>
   )
-}
+};
 const renderGrid = (grid) => {
   
-  const populateArrow = x => y => grid.arrows.filter(arrow => arrow.x===x && arrow.y===y);
+  const populateArrow = y => x => grid.arrows.filter(arrow => arrow.x===x && arrow.y===y);
   const populateRow = (row, index) => row.map(populateArrow(index));
 
-  const populatedGrid = grid.rows.map(populateRow);
+  const populatedGrid = getRows(grid.size).map(populateRow);
   return populatedGrid.map(renderRow);
 };
+// const onChangeSize = (event) => 
 
-const Application = (grid) => (
+export class Application extends React.Component { 
+
+constructor(props) {
+  super(props);
+
+  this.state = {
+    gridSize: 10,
+    numberOfArows: 10,
+    grid: newGrid(10, 10),
+    playing: true
+  }
+  this.newSizeHandler = this.newSize.bind(this);
+  this.newNumberOfArrowsHandler = this.newNumberOfArrows.bind(this);
+  this.nextGridHandler = this.nextGrid.bind(this);
+  this.newGridHandler = this.newGrid.bind(this);
+  this.playHandler = this.play.bind(this);
+  this.pauseHandler = this.pause.bind(this);
+}
+
+componentDidMount() {
+  this.playHandler();
+}
+play() {
+  this.timerID = setInterval(
+    () => this.nextGridHandler(),
+    500
+  );
+  {playing:true}
+}
+pause() {
+  clearInterval(this.timerID);
+  this.setState({playing:false});
+}
+newSize(e) {
+  this.setState({
+    gridSize: parseInt(e.target.value)
+  })
+}
+newNumberOfArrows(e) {
+  this.setState({
+    numberOfArows: parseInt(e.target.value)
+  })
+}
+nextGrid() {
+  this.setState({
+    grid: nextGrid(this.state.grid)
+  })
+}
+newGrid() {
+  this.setState({
+    grid: newGrid(this.state.gridSize, this.state.numberOfArows)
+  })
+}
+render() {
+  
+  return(
   <div>
     <h1>Arrows</h1>
     <br/>
+    <p>Size:</p>
+    <input type='number' onChange={this.newSizeHandler}/>
+    <br/>
+    <p>Number:</p>
+    <input type='number' onChange={this.newNumberOfArrowsHandler}/>
+    <br/>
+    <button onClick={this.newGridHandler}>
+      Reset
+    </button>
+    <button onClick={this.nextGridHandler}>
+      Next
+    </button>
+    <button disabled={this.state.playing} onClick={this.playHandler}>
+      Play
+    </button>
+    <button onClick={this.pauseHandler}>
+      Pause
+    </button>
     <br/>
     <table>
       <tbody>
-        {renderGrid(grid)}
+        {renderGrid(this.state.grid)}
       </tbody>
     </table>
   </div>
-)
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+)};
 }
+// function sleep(ms) {
+//   return new Promise(resolve => setTimeout(resolve, ms));
+// }
 
-async function demo() {
-  let cyclingGrid = seedGrid();
-  while(true){
-    cyclingGrid = nextGrid(cyclingGrid);
-    await sleep(1000);
-    ReactDOM.render(Application(cyclingGrid), document.getElementById('root'));
+// async function demo() {
+//   let cyclingGrid = seedGrid();
+//   while(true){
+//     cyclingGrid = nextGrid(cyclingGrid);
+//     await sleep(500);
+//     ReactDOM.render(Application(cyclingGrid), document.getElementById('root'));
+//   }
+// }
 
-  }
-}
+// demo();
 
-demo();
+ReactDOM.render(<Application/>, document.getElementById('root'));
 
